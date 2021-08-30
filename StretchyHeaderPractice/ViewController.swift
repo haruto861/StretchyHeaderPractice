f746dcf6a76d08574ac25a4fc8204b1694160c23
haruto-matsushima <matsu_haruto@icloud.com>
2021-08-31 03:04:27 +0900

    inital commit

---
+++ StretchyHeaderPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StretchyHeaderPractice
+//
+//  Created by 松島悠人 on 2021/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
